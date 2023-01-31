e6636c44c3700b29caad346ec0f597c1783ad078
Kristoffer Griessmann <kristoffergriessmann@gmail.com>
2023-01-30 19:31:00 -0800

    added questions in comments

--- my_prework.py
+++ my_prework.py
@@ -1,10 +1,12 @@
 # Question 1
+# Write a function to print "hello_USERNAME!" USERNAME is the input of the function.
 def hello_name(user_name):
     print('hello_' + user_name)
 user_name = input('Enter USERNAME: ')
 hello_name(user_name)
 
 # Question 2
+# Write a python function, first_odds that prints the odd numbers from 1-100 and returns nothing
 def first_odds():
     for x in range(1, 101):
         if x % 2 != 0:
@@ -12,6 +14,7 @@ def first_odds():
 first_odds()
 
 #  Question 3
+# write a Python function, max_num_in_list to return the max number of a given list
 def max_num_in_list(a_list):
     maximum = a_list[0]
     for i in range(len(a_list)):
@@ -22,6 +25,7 @@ a_list = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 print(max_num_in_list(a_list))
 
 #  Question 4
+# Write a function to return if the given year is a leap year
 def is_leap_year(a_year):
     if a_year % 400 == 0:
         return True
@@ -35,6 +39,7 @@ a_year = int(input())
 print(is_leap_year(a_year))
 
 # Question 5
+#Write a function to check to see if all numbers in list are consecutive numbers.
 def is_consecutive(a_list):
     return a_list == list(range(min(a_list), max(a_list)+1))
 
